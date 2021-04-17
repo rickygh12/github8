59f98a6e6e80206ffa94774681a36db9f9969d4e
RVC Student <c.konkol@rockvalleycollege.edu>
2021-04-17 09:08:18 -0500

    rg2

--- app/src/main/java/edu/rockvalleycollege/github7/MainActivity.kt
+++ app/src/main/java/edu/rockvalleycollege/github7/MainActivity.kt
@@ -1,9 +1,7 @@
 package edu.rockvalleycollege.github7
 // ricardo gordiano
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
